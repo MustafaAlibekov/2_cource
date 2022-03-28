5eaca56a088f13bd0aeaee44a529ce109ca768cd
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2022-03-28 12:12:45 +0300

    Add files via upload

---
+++ 1/ClassLibrary3/ClassLibrary3/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassLibrary3
+{
+    public static class Class1
+    {
+        
+        public static double MinAVG(string[] Marks)
+        {
+            int[] intMarks = new int[Marks.Length];
+            double a = 0;
+            double b = 0;
+            for (int i = 0; i < Marks.Length; i++)
+            {
+                Console.WriteLine(Marks[i]);
+                intMarks[i] = Convert.ToInt32(Marks[i]);
+                a = a + intMarks[i];
+                Console.WriteLine(a);
+            }
+            b = a / intMarks.Length;
+            Console.WriteLine(b);
+            return b;
+        }
+
+        public static string GetStudNumber(int year, int group, string fio)
+        {
+            string _year = Convert.ToString(year);
+            string _group = Convert.ToString(group);
+            string[] _fio = fio.Split(' ');
+            string name = _fio[0];
+            string surname = _fio[1];
+            string otchstvo = _fio[2];
+            string vozvrat = _year + '.' + _group + '.' + Convert.ToString(name[0]) + Convert.ToString(surname[0]) + Convert.ToString(otchstvo[0]);
+            Console.WriteLine(vozvrat);
+            return vozvrat;
+        }
+    }
+}
---
+++ 1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary3;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Class1.
+        }
+    }
+}
